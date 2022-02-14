e9764e30985021f45150104860acd380c67dddbc
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2022-02-14 21:52:41 +0545

    [v1.0.0] Clientside features

---
+++ docs/_nuxt/static/1644854783/create-token/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/create-token", {data:[{},{}],fetch:{},mutations:[]});
---
+++ docs/_nuxt/static/1644854783/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fadmin","\u002Ftokensale","\u002Ftoken","\u002Fcreate-token"]})
